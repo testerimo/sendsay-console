2ab3af12b762dc8a2673bff1e4b88dc7b7c53ff1
Misha Trifonov <hello@testerimo.com>
2020-09-04 00:28:34 +0300

    Updates

---
+++ precache-manifest.4af1bd583bd55cb0afbf1817378b39c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d12f2dd4568558e86decf6cd7adfc5b",
+    "url": "/sendsay-console/index.html"
+  },
+  {
+    "revision": "3fd31dd2b1b65db29dcc",
+    "url": "/sendsay-console/static/css/main.53de21ef.chunk.css"
+  },
+  {
+    "revision": "fe7326877d1d0ed5ae11",
+    "url": "/sendsay-console/static/js/2.0c9dee3b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/sendsay-console/static/js/2.0c9dee3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fd31dd2b1b65db29dcc",
+    "url": "/sendsay-console/static/js/main.f86790ca.chunk.js"
+  },
+  {
+    "revision": "b72f11c52e896a3bb887",
+    "url": "/sendsay-console/static/js/runtime-main.1b90907c.js"
+  },
+  {
+    "revision": "c6b12ac8dcd573bcf337adc092e9fe35",
+    "url": "/sendsay-console/static/media/meh.c6b12ac8.svg"
+  }
+]);
